371fbdb98d70e277dfba176779bcfeeaf18d0d3a
Engjell <40763@edu.raseko.fi>
2021-03-14 16:21:11 +0200

    dynamic relations

--- app.js
+++ app.js
@@ -12,6 +12,35 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = (eventIds) => {
+  return Event.find({ _id: { $in: eventIds } })
+    .then((events) => {
+      return events.map((event) => {
+        return {
+          ...event._doc,
+          _id: event.id,
+          creator: user.bind(this, event.creator),
+        };
+      });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+const user = (userId) => {
+  return User.findById(userId)
+    .then((user) => {
+      return {
+        ...user._doc,
+        _id: user.id,
+        createdEvents: events.bind(this, user._doc.createdEvents),
+      };
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
 app.use(
   "/graphql",
   graphqlHttp({
@@ -22,12 +51,14 @@ app.use(
             description: String!
             price: Float!
             date: String!
+            creator: User!
         }
 
         type User {
           _id: ID!
           email: String!
           password: String
+          createdEvents: [Event!]
         }
 
         input EventInput {
@@ -57,9 +88,14 @@ app.use(
     rootValue: {
       events: () => {
         return Event.find()
+          .populate("creator")
           .then((events) => {
             return events.map((event) => {
-              return { ...event._doc, _id: event.id };
+              return {
+                ...event._doc,
+                _id: event.id,
+                creator: user.bind(this, event._doc.creator),
+              };
             });
           })
           .catch((err) => {
