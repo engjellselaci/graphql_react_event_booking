0b5dc37ac66b983de9c85e2f49f6723e4924736f
Engjell <40763@edu.raseko.fi>
2021-03-17 13:25:01 +0200

    user authentication

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
 const graphQlSchema = require("./graphql/schema/index");
 const graphqlResolvers = require("./graphql/resolvers/index");
+const isAuth = require("./middleware/is-auth");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(isAuth);
 
 app.use(
   "/graphql",
@@ -17,6 +20,7 @@ app.use(
     graphiql: true,
   })
 );
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.yodiq.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
