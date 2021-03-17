6df7d08e8c4d2c7c72f84b1efaf818d14e9c81e4
Engjell <40763@edu.raseko.fi>
2021-03-17 18:33:12 +0200

    navbar init and styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,22 @@ import "./App.css";
 import Auth from "./pages/Auth";
 import Bookings from "./pages/Bookings";
 import Events from "./pages/Events";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={Auth} />
-        <Route path="/events" component={Events} />
-        <Route path="/bookings" component={Bookings} />
-      </Switch>
+      <React.Fragment>
+        <MainNavigation />
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/auth" exact />
+            <Route path="/auth" component={Auth} />
+            <Route path="/events" component={Events} />
+            <Route path="/bookings" component={Bookings} />
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
